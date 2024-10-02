ae24e35ff6e48916007adba338ac46b38e635133
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-10-03 00:12:29 +0200

    solved task copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,6 +1,15 @@
+import fs from "node:fs";
 
 const copy = async () => {
-  
+  if (fs.existsSync("./files_copy") || !fs.existsSync("./files")) {
+    throw new Error("FS operation failed");
+  } else {
+    fs.cp("./files", "./files_copy", { recursive: true }, (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -3,11 +3,11 @@ import fs from "node:fs";
 const create = async () => {
   const content = "I am fresh and young";
   if (fs.existsSync("./files/fresh.txt")) {
-    console.error("FS operation failed");
+    throw new Error("FS operation failed");
   } else {
     fs.writeFile("./files/fresh.txt", content, (err) => {
       if (err) {
-        console.error(err);
+        throw new Error(err);
       }
     });
   }
