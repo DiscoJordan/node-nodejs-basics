6ddb8c541dfb0427d62517adc80ede74b5307127
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-10-03 00:35:45 +0200

    solved task rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import fs from "node:fs";
+
 const rename = async () => {
-    // Write your code here 
+  if (
+    fs.existsSync("./files/properFilename.md") ||
+    !fs.existsSync("./files/wrongFilename.txt")
+  ) {
+    throw new Error("FS operation failed");
+  } else
+    fs.rename(
+      "./files/wrongFilename.txt",
+      "./files/properFilename.md",
+      function (err) {
+        if (err) {
+          throw new Error(err);
+        }
+      }
+    );
 };
 
 await rename();
