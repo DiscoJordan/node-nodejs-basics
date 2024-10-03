a1cc323f6e212d1cf3bc3685c9047ce00f706426
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-10-03 14:57:52 +0200

    refactored delete.js solution withot sync methods

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,15 +1,17 @@
 import fs from "node:fs";
 
 const remove = async () => {
-  if (!fs.existsSync("./files/fileToRemove.txt")) {
-    throw new Error("FS operation failed");
-  } else {
-    fs.unlink("./files/fileToRemove.txt", function (err) {
-      if (err) {
-        throw new Error(err);
-      }
-    });
-  }
+  fs.stat("./files/fileToRemove.txt", function (err) {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.unlink("./files/fileToRemove.txt", function (err) {
+        if (err) {
+          throw new Error(err);
+        }
+      });
+    }
+  });
 };
 
 await remove();
