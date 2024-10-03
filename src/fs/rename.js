fe3c3dddde06e92b6f40d59ed957c1ae77b40392
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-10-03 14:54:59 +0200

    refactored rename.js solution withot sync methods

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,21 +1,27 @@
 import fs from "node:fs";
 
 const rename = async () => {
-  if (
-    fs.existsSync("./files/properFilename.md") ||
-    !fs.existsSync("./files/wrongFilename.txt")
-  ) {
-    throw new Error("FS operation failed");
-  } else
-    fs.rename(
-      "./files/wrongFilename.txt",
-      "./files/properFilename.md",
-      function (err) {
+  fs.stat("./files/wrongFilename.txt", function (err) {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.stat("./files/properFilename.md", function (err) {
         if (err) {
-          throw new Error(err);
+          fs.rename(
+            "./files/wrongFilename.txt",
+            "./files/properFilename.md",
+            function (err) {
+              if (err) {
+                throw new Error(err);
+              }
+            }
+          );
+        } else {
+          throw new Error("FS operation failed");
         }
-      }
-    );
+      });
+    }
+  });
 };
 
 await rename();
