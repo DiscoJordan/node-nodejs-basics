99d0a5d2f41d4aec5a1e13b673b1cb8290b41507
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-10-03 15:00:04 +0200

    refactored list.js solution without sync methods

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,19 +1,21 @@
 import fs from "node:fs";
 
 const list = async () => {
-  if (!fs.existsSync("./files")) {
-    throw new Error("FS operation failed");
-  } else
-    fs.readdir("./files", (err, files) =>
-      files.forEach((file) => {
-        {
-          console.log(file);
-          if (err) {
-            throw new Error(err);
+  fs.stat("./files", function (err) {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else
+      fs.readdir("./files", (err, files) =>
+        files.forEach((file) => {
+          {
+            console.log(file);
+            if (err) {
+              throw new Error(err);
+            }
           }
-        }
-      })
-    );
+        })
+      );
+  });
 };
 
 await list();
