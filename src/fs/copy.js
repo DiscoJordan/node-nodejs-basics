2c7c530e829aeb1f9fd81324871d745a27008b60
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-10-03 14:49:42 +0200

    refactored copy.js solution withot sync methods

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,15 +1,25 @@
 import fs from "node:fs";
 
 const copy = async () => {
-  if (fs.existsSync("./files_copy") || !fs.existsSync("./files")) {
-    throw new Error("FS operation failed");
-  } else {
-    fs.cp("./files", "./files_copy", { recursive: true }, (err) => {
-      if (err) {
-        throw new Error(err);
-      }
-    });
-  }
+  fs.stat("./files_copy", function (err) {
+    if (err) {
+      fs.stat("./files", function (err) {
+        if (err) {
+          throw new Error("FS operation failed");
+        } else {
+          fs.cp("./files", "./files_copy", { recursive: true }, (err) => {
+            if (err) {
+              throw new Error(err);
+            }
+          });
+        }
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
+
+  
 };
 
 await copy();
