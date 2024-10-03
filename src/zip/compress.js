f03abea94991b22d4c16ae6a922fa5761e05619a
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-10-03 20:21:34 +0200

    solved task compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream("./files/fileToCompress.txt");
+  const destination = createWriteStream("./files/archive.gz");
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
