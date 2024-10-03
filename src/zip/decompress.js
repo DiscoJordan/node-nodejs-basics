b51f8ba502a3cdac876ee046904b207a063cbaca
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-10-03 20:29:26 +0200

    finished block zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import { createGunzip } from "node:zlib";
+import { pipeline } from "node:stream";
+import { createReadStream, createWriteStream } from "node:fs";
 
+const decompress = async () => {
+  const gunzip = createGunzip();
+  const source = createReadStream("./files/archive.gz");
+  const destination = createWriteStream("./files/fileToCompress.txt");
+
+  pipeline(source, gunzip, destination, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
+};
 await decompress();
