4c1fb930336568d65c5537c9874333b9215c0ada
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-10-03 15:16:52 +0200

    solved task env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+  const newKeys = Object.keys(process.env).map((key) => "RSS_" + key);
+  newKeys.forEach((key, id) =>
+    console.log(key + "=" + Object.values(process.env)[id] + ";")
+  );
 };
 
 parseEnv();
