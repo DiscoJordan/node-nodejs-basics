4f2baed2bb9d21ea7d03aff2e5590a9bb698c777
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-10-13 13:34:10 +0200

    fork on mistakes

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -1,6 +1,7 @@
 const args = process.argv.slice(2);
 
 console.log(`Total number of arguments is ${args.length}`);
+
 console.log(`Arguments: ${JSON.stringify(args)}`);
 
 const echoInput = (chunk) => {
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,9 +1,11 @@
+
 import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PathTo = path.resolve(__dirname, "files", "fresh.txt");
+
 
 const greeting = "I am fresh and young";
 
